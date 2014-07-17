c58ba2fb3164ca97c3a6c62192adeeb737b0c4d8
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-17 15:16:14 -0800

    basic java calculation

---
+++ basicCalculation.java
@@ -0,0 +1,12 @@
+
+public class basicCalculation {
+	public static void main(String args[])
+	{
+		double r, pi, a;
+		r = 2;
+		pi = 3.1416;
+		a = pi * r * r;
+		
+		System.out.println("Area of the triangle is "+a);
+	}
+}
