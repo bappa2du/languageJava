50b86523f5bf6d8c550b91b47c166c0d9c0c3651
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-17 15:58:32 -0800

    java foreach loop

---
+++ forEach.java
@@ -0,0 +1,16 @@
+
+public class forEach {
+	public static void main(String args[])
+	{
+		int nums[] = {1,2,3,4,5,6,7,8,9};
+		int sum = 0;
+		
+		for(int x:nums)
+		{
+			System.out.println("Value is "+x);
+			sum += x;
+		}
+		
+		System.out.println("Summation is "+sum);
+	}
+}
