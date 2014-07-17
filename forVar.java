795d526dbd2a1d5b7bb5eceb6da0032101b8ec15
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-17 15:55:26 -0800

    java for loop version

---
+++ forVar.java
@@ -0,0 +1,17 @@
+
+public class forVar {
+	public static void main(String args[])
+	{
+		int i;
+		boolean done = false;
+		
+		i = 0;
+		
+		for( ;!done; )
+		{
+			System.out.println("i is "+i);
+			if(i==10) done = true;
+			i++;
+		}
+	}
+}
