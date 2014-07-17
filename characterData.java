34228900c59db5117f2ef8ff8a9dfd19c3dd400c
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-17 15:34:23 -0800

    java characterdata basic

---
+++ characterData.java
@@ -0,0 +1,14 @@
+
+public class characterData {
+	public static void main(String args[])
+	{
+		char ch1,ch2;
+		
+		ch1 = 'X';
+		ch2 = 80;
+		ch1++;
+		ch2++;
+		
+		System.out.println(ch1);
+	}
+}
