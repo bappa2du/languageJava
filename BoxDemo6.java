42050667dd4598469e0cf1673e63d48b42bdb767
Iftakharul Alam <iftakharul.alam@live.com>
2014-07-17 18:15:05 -0800

    java simple constructor

---
+++ BoxDemo6.java
@@ -0,0 +1,36 @@
+class Boxnew
+{
+	double width;
+	double height;
+	double depth;
+	
+	Boxnew()
+	{
+		System.out.println("Constructing Box..");
+		width = 10;
+		height = 10;
+		depth = 10;
+	}
+	
+	double volume()
+	{
+		return width * height * depth;
+	}
+}
+
+class BoxDemo6
+{
+	public static void main(String args[])
+	{
+		Boxnew mybox1 = new Boxnew();
+		Boxnew mybox2 = new Boxnew();
+		
+		double vol;
+		
+		vol = mybox1.volume();
+		System.out.println("Volume is "+vol);
+		vol = mybox2.volume();
+		System.out.println("Volume is "+vol);
+
+	}
+}
